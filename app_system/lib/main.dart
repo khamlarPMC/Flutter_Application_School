06329f4a7913be42b8d1ac5ee62f2f3c941727dd
White_album <113410310+khamlarPMC@users.noreply.github.com>
2023-07-18 08:57:15 +0700

    update widget
    in file main.dart delete class MyApp change MyApp to HomeScreen
    in file testwidget.dart update change MyApp to HomeScreen

--- app_system/lib/main.dart
+++ app_system/lib/main.dart
@@ -1,21 +1,8 @@
 import 'package:app_system/pages/home.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(HomeScreen());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'Your App',
-      home: HomeScreen(),
-      // Add any other necessary configuration options
-    );
-  }
-}
 
